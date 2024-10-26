d3ed8a5e12df5b294a278dbd21f25ab6e1df7916
Samuel tetenga <126316270+hendrckks@users.noreply.github.com>
2024-10-26 15:53:23 +0300

    add: scroll to top

--- src/app/MainLayout.tsx
+++ src/app/MainLayout.tsx
@@ -3,12 +3,14 @@ import AuthPopup from "../components/authmodals/AuthModal";
 import { useAuth } from "../context/AuthContext";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 const MainLayout = () => {
   const { showAuthModal } = useAuth();
 
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Navbar />
       <main className="flex-1 pt-24">
         <Outlet />
